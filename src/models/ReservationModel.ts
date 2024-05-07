f6242ceddf749bffcc4f01fa5ad0c3ba6f2a7c1b
Clément Deligny <clement.deligny@tutamail.com>
2024-05-07 16:16:38 +0200

    interface en cours

--- src/models/ReservationModel.ts
+++ src/models/ReservationModel.ts
@@ -24,4 +24,110 @@ export interface ReservationModel {
 export default class BookingModel {
     private _formdata: any;
     private _formresult: ReservationModel;
+
+    constructor (formData: any) {
+        this._formdata = formData;
+        this._formresult = {
+            lastName: this.checkLastName(),
+            firstName: this.checkFirstName(),
+            email: this.checkEmail(),
+            cellphone: this.checkCellphone(),
+            eventNature: this.checkEventNature(),
+            eventDate: this.checkEventDate(),
+            eventHour: this.checkEventHour(),
+            eventDistrict: this.checkEventDistrict(),
+            eventPeople: this.checkEventPeople(),
+            eventAgeAverage: this.checkEventAgeAverage(),
+            eventPreferredCity: this.checkEventPreferredCity(),
+            eventVenue: this.checkEventVenue(),
+            eventType: this.checkEventType(),
+            eventCaterer: this.checkEventCaterer(),
+            eventQuotation: this.checkEventQuotation(),
+            eventBudget: this.checkEventBudget(),
+            eventAccomodation: this.checkEventAccomodation(),
+            eventBedding: this.checkEventBedding(),
+            eventDetails: this.checkEventDetails(),
+            cguChecked: this.checkCguCheck(),
+        }
+    }
+
+    private checkLastName(): string {
+        
+    }
+
+    private checkFirstName(): string {
+
+    }
+
+    private checkEmail(): string {
+
+    }
+
+    private checkCellphone(): number {
+
+    }
+
+    private checkEventNature(): string {
+
+    }
+
+    private checkEventDate(): Date {
+
+    }
+
+    private checkEventHour(): any{// trouver le type
+
+    }
+
+    private checkEventDistrict(): string {
+
+    }
+
+    private checkEventPeople(): number {
+
+    }
+
+    private checkEventAgeAverage(): string {
+
+    }
+
+    private checkEventPreferredCity(): string {
+
+    }
+
+    private checkEventVenue(): string {
+
+    }
+
+    private checkEventType(): string {
+
+    }
+
+    private checkEventCaterer(): string {
+
+    }
+
+    private checkEventQuotation(): string {
+
+    }
+
+    private checkEventBudget(): number {
+
+    }
+
+    private checkEventAccomodation(): string {
+
+    }
+
+    private checkEventBedding(): number {
+
+    }
+
+    private checkEventDetails(): string {
+
+    }
+
+    private checkCguCheck(): boolean {
+        
+    }
 }
