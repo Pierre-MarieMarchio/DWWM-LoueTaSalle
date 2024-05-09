871b98650cd7701d9044f6aa954c32d87ba09d01
Clément <clement.universe@gmail.com>
2024-05-09 17:44:33 +0200

    fonction vérification LastName faite

--- src/models/ReservationModel.ts
+++ src/models/ReservationModel.ts
@@ -1,133 +1,101 @@
 export interface ReservationModel {
-    lastName: string;
-    firstName: string;
-    email: string;
-    cellphone: number;
-    eventNature: string; 
-    eventDate: Date;
-    eventHour: string; 
-    eventDistrict: string; 
-    eventPeople: number;
-    eventAgeAverage: string; 
-    eventPreferredCity: string; 
-    eventVenue: string; 
-    eventType: string; 
-    eventCaterer: string; 
-    eventQuotation: string; 
-    eventBudget: number;
-    eventAccomodation: string; 
-    eventBedding: number;
-    eventDetails: string;
-    cguChecked: boolean; 
+  lastName: string;
+  firstName: string;
+  email: string;
+  cellphone: number;
+  eventNature: string;
+  eventDate: Date;
+  eventHour: string;
+  eventDistrict: string;
+  eventPeople: number;
+  eventAgeAverage: string;
+  eventPreferredCity: string;
+  eventVenue: string;
+  eventType: string;
+  eventCaterer: string;
+  eventQuotation: string;
+  eventBudget: number;
+  eventAccomodation: string;
+  eventBedding: number;
+  eventDetails: string;
+  cguChecked: boolean;
 }
 
 export default class BookingModel {
-    private _formdata: any;
-    private _formresult: ReservationModel;
+  private _formdata: any;
+  private _formresult: ReservationModel;
 
-    constructor (formData: any) {
-        this._formdata = formData;
-        this._formresult = {
-            lastName: this.checkLastName(),
-            firstName: this.checkFirstName(),
-            email: this.checkEmail(),
-            cellphone: this.checkCellphone(),
-            eventNature: this.checkEventNature(),
-            eventDate: this.checkEventDate(),
-            eventHour: this.checkEventHour(),
-            eventDistrict: this.checkEventDistrict(),
-            eventPeople: this.checkEventPeople(),
-            eventAgeAverage: this.checkEventAgeAverage(),
-            eventPreferredCity: this.checkEventPreferredCity(),
-            eventVenue: this.checkEventVenue(),
-            eventType: this.checkEventType(),
-            eventCaterer: this.checkEventCaterer(),
-            eventQuotation: this.checkEventQuotation(),
-            eventBudget: this.checkEventBudget(),
-            eventAccomodation: this.checkEventAccomodation(),
-            eventBedding: this.checkEventBedding(),
-            eventDetails: this.checkEventDetails(),
-            cguChecked: this.checkCguCheck(),
-        }
+  constructor(formData: any) {
+    this._formdata = formData;
+    this._formresult = {
+      lastName: this.checkLastName(),
+      firstName: this.checkFirstName(),
+      email: this.checkEmail(),
+      cellphone: this.checkCellphone(),
+      eventNature: this.checkEventNature(),
+      eventDate: this.checkEventDate(),
+      eventHour: this.checkEventHour(),
+      eventDistrict: this.checkEventDistrict(),
+      eventPeople: this.checkEventPeople(),
+      eventAgeAverage: this.checkEventAgeAverage(),
+      eventPreferredCity: this.checkEventPreferredCity(),
+      eventVenue: this.checkEventVenue(),
+      eventType: this.checkEventType(),
+      eventCaterer: this.checkEventCaterer(),
+      eventQuotation: this.checkEventQuotation(),
+      eventBudget: this.checkEventBudget(),
+      eventAccomodation: this.checkEventAccomodation(),
+      eventBedding: this.checkEventBedding(),
+      eventDetails: this.checkEventDetails(),
+      cguChecked: this.checkCguCheck(),
+    };
+  }
+
+  private checkLastName(): string {
+    if (typeof this._formresult.lastName === "string") {
+      return this._formresult.lastName;
+    } else {
+      throw new Error("n'est pas un string");
     }
+  }
 
-    private checkLastName(): string {
-        
-    }
+  private checkFirstName(): string {}
 
-    private checkFirstName(): string {
+  private checkEmail(): string {}
 
-    }
+  private checkCellphone(): number {}
 
-    private checkEmail(): string {
+  private checkEventNature(): string {}
 
-    }
+  private checkEventDate(): Date {}
 
-    private checkCellphone(): number {
+  private checkEventHour(): any {
+    // trouver le type
+  }
 
-    }
+  private checkEventDistrict(): string {}
 
-    private checkEventNature(): string {
+  private checkEventPeople(): number {}
 
-    }
+  private checkEventAgeAverage(): string {}
 
-    private checkEventDate(): Date {
+  private checkEventPreferredCity(): string {}
 
-    }
+  private checkEventVenue(): string {}
 
-    private checkEventHour(): any{// trouver le type
+  private checkEventType(): string {}
 
-    }
+  private checkEventCaterer(): string {}
 
-    private checkEventDistrict(): string {
+  private checkEventQuotation(): string {}
 
-    }
+  private checkEventBudget(): number {}
 
-    private checkEventPeople(): number {
+  private checkEventAccomodation(): string {}
 
-    }
+  private checkEventBedding(): number {}
 
-    private checkEventAgeAverage(): string {
+  private checkEventDetails(): string {}
 
-    }
-
-    private checkEventPreferredCity(): string {
-
-    }
-
-    private checkEventVenue(): string {
-
-    }
-
-    private checkEventType(): string {
-
-    }
-
-    private checkEventCaterer(): string {
-
-    }
-
-    private checkEventQuotation(): string {
-
-    }
-
-    private checkEventBudget(): number {
-
-    }
-
-    private checkEventAccomodation(): string {
-
-    }
-
-    private checkEventBedding(): number {
-
-    }
-
-    private checkEventDetails(): string {
-
-    }
-
-    private checkCguCheck(): boolean {
-        
-    }
+  private checkCguCheck(): boolean {}
 }
